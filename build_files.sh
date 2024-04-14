6fdaa19e85d177bd750adbf9fa8a76d6bfd8b92e
Akeem Sanyaolu <akeemsanyaolu2@gmail.com>
2024-04-14 21:16:44 +0100

    fix 2

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
-python3 -m ensurepip --upgrade
-python3.9 -m pip install --upgrade pip
+# python3 -m ensurepip --upgrade
+# python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic
